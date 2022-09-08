0a8636d7cbdd9c23cb9bf203c56fd68c0e966261
Gauri Nalawade <gaurinalawade84@gmail.com>
2022-09-08 09:16:05 +0530

    script

--- Save Text As File/script.js
+++ Save Text As File/script.js
@@ -4,6 +4,17 @@ selectMenu = document.querySelector(".save-as select"),
 saveBtn = document.querySelector(".save-btn");
 
 saveBtn.addEventListener("click", () =>{
-      console.log(textarea.value)
+      const blob = new Blob([textarea.value], {type: selectMenu.value});
+      // URL.createobject creates a url that represent passed object
+      const fileUrl = URL.createObjectURL(blob);
+      // creating <a> tag
+      const link = document.createElement("a");
+      // passing filename as download value of link
+      link.download = fileNameInput.value;
+      // passing fileURL as href value of link
+      link.href = fileUrl;
+      // clicking link so the file download
+      link.click()
+
 })
 
